acf4e35083a4559cbde9876e988ffd4558c310ac
AlisonBrito <165394229+AlisonBrito@users.noreply.github.com>
2024-10-24 21:39:09 -0300

    .

--- projetoTrabalho/FormLogin.cs
+++ projetoTrabalho/FormLogin.cs
@@ -21,6 +21,37 @@ namespace projetoTrabalho
         GerenciadorBD gbd = new GerenciadorBD();
         private void button1_Click(object sender, EventArgs e)
         {
+  
+        } 
+    
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                FormCadastro fcf = new FormCadastro();
+                fcf.ShowDialog(); 
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void FormLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+
+            if (e.KeyCode == Keys.Enter)
+            {
             string connection = gbd.getConnectionString();
 
             MySqlConnection conn = new MySqlConnection(connection);
@@ -51,7 +82,7 @@ namespace projetoTrabalho
                         break;
                     }
                 }
-                if(nomeOK && senhaOK)
+                    if (nomeOK && senhaOK)
                 {
                     MessageBox.Show("Bem vindo ao sistema");
                 }
@@ -65,31 +96,52 @@ namespace projetoTrabalho
 
                 MessageBox.Show(ex.Message);
             }
+            FormPosLogin fcf = new FormPosLogin();
+            fcf.ShowDialog();
+            }
+        }
 
-            /*
-            string usuario = "abc", senha = "dfg", retorno = "";
-            bool verificaLogin = false;
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string connection = gbd.getConnectionString();
                 
-            if(textBox1.Text != usuario)
-            {
-                retorno += "O usuario esta incorreto\n";
-                verificaLogin = true;
-            }
+                MySqlConnection conn = new MySqlConnection(connection);
+                string nome = textBox1.Text;
+                string senha = textBox2.Text;
+                bool nomeOK = false;
+                bool senhaOK = false;
 
-            if(textBox2.Text != senha) 
+                try
             {
-                retorno += "A senha esta incorreta";
-                verificaLogin = true;
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "select * from funcionarios";
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (nome == reader.GetValue(1).ToString())
+                        {
+                            nomeOK = true;
             }
-
-            if(verificaLogin) 
+                        if (nomeOK)
             {
-                MessageBox.Show("Algo deu errado: \n" + retorno);
+                            if (senha == reader.GetValue(2).ToString())
+                            {
+                                senhaOK = true;
+            }
+                            break;
+                        }
+                    }
+                    if (nomeOK && senhaOK)
+            {
+                        MessageBox.Show("Bem vindo ao sistema");
             }
             else
             {
-                MessageBox.Show("Bem Vindo!");
-            }*/
+                        MessageBox.Show("Dados Incorretos");
         } 
     
 
@@ -98,10 +150,14 @@ namespace projetoTrabalho
                 FormCadastro fcf = new FormCadastro();
                 fcf.ShowDialog(); 
         }
-
-        private void FormLogin_Load(object sender, EventArgs e)
+                catch (Exception ex)
         {
 
+                    MessageBox.Show(ex.Message);
+                }
+                FormPosLogin fcf = new FormPosLogin();
+                fcf.ShowDialog();
+            }
         }
     }
 }  
